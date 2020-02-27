df2ad98737b59e11f89b9475828f22f0ab5b1224
howardC56 <h2chang56@gmail.com>
2020-02-27 10:20:13 -0500

    added tableView view

---
+++ FoursquareGroupProject/FoursquareGroupProject/Extensions/Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  Extensions.swift
+//  FoursquareGroupProject
+//
+//  Created by Howard Chang on 2/26/20.
+//  Copyright © 2020 Melinda Diaz. All rights reserved.
+//
+
+import Foundation
---
+++ FoursquareGroupProject/FoursquareGroupProject/Views/VenueTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  VenueTableViewCell.swift
+//  FoursquareGroupProject
+//
+//  Created by Howard Chang on 2/26/20.
+//  Copyright © 2020 Melinda Diaz. All rights reserved.
+//
+
+import UIKit
+
+class VenueTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
