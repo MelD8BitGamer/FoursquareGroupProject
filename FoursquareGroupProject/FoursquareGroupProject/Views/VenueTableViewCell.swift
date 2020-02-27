1d6f974ca063f7fe041a617a397882fa1bf9b70e
howardC56 <h2chang56@gmail.com>
2020-02-27 10:26:37 -0500

    tableViewCell

--- FoursquareGroupProject/FoursquareGroupProject/Views/VenueTableViewCell.swift
+++ FoursquareGroupProject/FoursquareGroupProject/Views/VenueTableViewCell.swift
@@ -8,17 +8,71 @@
 
 import UIKit
 
-class VenueTableViewCell: UITableViewCell {
+final class VenueTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    public lazy var venueImageView: UIImageView = {
+        let imageV = UIImageView()
+        imageV.clipsToBounds = true
+        imageV.layer.cornerRadius = 10
+        return imageV
+    }()
+    
+    public lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .boldSystemFont(ofSize: 18)
+        label.textAlignment = .center
+        label.adjustsFontSizeToFitWidth = true
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    lazy var VenueAddress: UILabel = {
+        let descriptionLabel = UILabel()
+        descriptionLabel.font = .boldSystemFont(ofSize: 16)
+        descriptionLabel.textAlignment = .center
+        descriptionLabel.adjustsFontSizeToFitWidth = true
+        descriptionLabel.numberOfLines = 0
+        return descriptionLabel
+    }()
+    
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super .init(style: style, reuseIdentifier: reuseIdentifier)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    private func commonInit() {
+        backgroundColor = .white
+        venueImageViewConstraints()
+        titleLabelConstraints()
+        venueAddressLabelConstraints()
     }
-
+    
+    private func venueImageViewConstraints() {
+        addSubview(venueImageView)
+        venueImageView.translatesAutoresizingMaskIntoConstraints = false
+        venueImageView.heightAnchor.constraint(equalToConstant: 80).isActive = true
+        venueImageView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        venueImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 12).isActive = true
+        venueImageView.widthAnchor.constraint(equalTo: venueImageView.heightAnchor, multiplier: 16/9).isActive = true
+    }
+    
+    private func titleLabelConstraints() {
+        addSubview(titleLabel)
+        titleLabel.anchor(top: topAnchor, left: venueImageView.rightAnchor, right: rightAnchor, paddingTop: 20, paddingLeft: 10, paddingRight: 8)
+    }
+    
+    private func venueAddressLabelConstraints() {
+        addSubview(VenueAddress)
+        VenueAddress.anchor(top: titleLabel.bottomAnchor, left: titleLabel.leftAnchor, right: titleLabel.rightAnchor, paddingTop: 8)
+    }
+    
+    func configureCell(venue: Venue) {
+        
+    }
+    
 }
