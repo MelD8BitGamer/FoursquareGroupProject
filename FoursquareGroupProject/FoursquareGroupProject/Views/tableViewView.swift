858ae59e2a1109d88fe2a80e153210e188567d73
howardC56 <h2chang56@gmail.com>
2020-02-28 01:41:15 -0500

    navigation fully functional

--- FoursquareGroupProject/FoursquareGroupProject/Views/tableViewView.swift
+++ FoursquareGroupProject/FoursquareGroupProject/Views/tableViewView.swift
@@ -9,15 +9,17 @@
 import UIKit
 
 final class tableViewView: UIView {
- 
-lazy var tableView: UITableView = {
-           let tableV = UITableView()
-           tableV.rowHeight = 100
-           tableV.backgroundColor = .white
-           return tableV
-       }()
+    
+    lazy var tableView: UITableView = {
+        let tableV = UITableView()
+        tableV.rowHeight = 100
+        tableV.backgroundColor = .white
+        return tableV
+    }()
+    
     lazy var topView: UILabel = {
         let button = UILabel()
+        button.isUserInteractionEnabled = true
         button.backgroundColor = UIColor.gray
         return button
     }()
@@ -27,24 +29,24 @@ lazy var tableView: UITableView = {
         addSubview(topView)
         topView.anchor(top: topAnchor, left: leftAnchor, right: rightAnchor, height: 25)
     }
-       
-      private func tableViewConstraints() {
-           addSubview(tableView)
+    
+    private func tableViewConstraints() {
+        addSubview(tableView)
         tableView.anchor(top: topView.bottomAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor)
-       }
-       
-      override init(frame: CGRect) {
-       super.init(frame: UIScreen.main.bounds)
-       backgroundColor = .blue
-       layer.cornerRadius = 20
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: UIScreen.main.bounds)
+        backgroundColor = .blue
+        layer.cornerRadius = 20
         layer.masksToBounds = true
         buttonConstraints()
-       tableViewConstraints()
-       tableView.register(VenueTableViewCell.self, forCellReuseIdentifier: "VenueTableViewCell")
-      
-   }
-       
-       required init?(coder: NSCoder) {
-           fatalError("init(coder:) has not been implemented")
-   }
+        tableViewConstraints()
+        tableView.register(VenueTableViewCell.self, forCellReuseIdentifier: "VenueTableViewCell")
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
